e50bd936b5c5172f009992a10262bc594ab459f1
jbenaventem <jesus.benavente@gruposantander.com>
2021-08-18 14:34:01 +0200

    Parser based on python

--- clair_to_junit_parser.py
+++ clair_to_junit_parser.py
@@ -47,7 +47,7 @@ def main():
             new_step.failure_type = "unapproved"
             new_step.failure_message = "Please have the following security issue reviewed: {}".format(clair_parsed_file['vulnerabilities'][idVulnerability]["links"])
             new_step.failure_output = clair_parsed_file['vulnerabilities'][idVulnerability]["description"]
-            logger.warning(TestSuite.to_xml_string(new_step))
+            logger.warning(new_step)
             current_suite.test_cases.append(new_step)
         # try to write new file
     try:
