0a081551d3a6079603e72b7387d94627648da1f8
jbenaventem <jesus.benavente@gruposantander.com>
2021-08-18 11:45:48 +0200

    Parser based on python

--- clair_to_junit_parser.py
+++ clair_to_junit_parser.py
@@ -37,7 +37,7 @@ def main():
     test_suites = []
     for each  in clair_parsed_file["vulnerabilities"]:
         logger.warning("name is  " + each)
-        logger.warning(each.['name'])
+        logger.warning([each]['name'])
 
 if __name__ == "__main__":
     main()
